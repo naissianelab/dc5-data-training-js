7b9928c6b0f704861591ad004da719a0a55edb74
naissianelab <naissianelabrosse@gmail.com>
2023-09-07 11:22:59 +0200

    mathematique

---
+++ echauffement/exo2.js
@@ -0,0 +1,12 @@
+let nombre1 = 20;
+let nombre2 = 5;
+
+let somme = nombre1 + nombre2;       // Addition
+let difference = nombre2 - nombre1;  // Soustraction
+let produit = nombre1 * nombre2;     // Multiplication
+let quotient = nombre1 / nombre2;    // Division
+
+console.log("Resultat de l'addition : " + somme);
+console.log("Resultat de la soustraction : " + difference);
+console.log("Resultat de la multiplication : " + produit);
+console.log("Resultat de la division : " + quotient);
