cc636fa4323f1799543239b67938ce6ede2feebe
naissianelab <naisianelabrosse@gmail.com>
2023-09-26 19:35:27 +0200

    est pair

--- Mobilisation Articulaire/exo3.js
+++ Mobilisation Articulaire/exo3.js
@@ -2,5 +2,4 @@ function exo3(nombre) {
     return nombre % 2 === 0;
 }
 
-console.log(estPair(3)); // Cela affichera "true" dans la console, car 4 est pair.
-console.log(estPair(7)); 
+console.log(estPair(2))
