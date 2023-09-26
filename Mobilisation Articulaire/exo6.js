8b371dab12d648df05196eac3e597373171db7f8
naissianelab <naisianelabrosse@gmail.com>
2023-09-26 19:50:35 +0200

    rectangle

---
+++ Mobilisation Articulaire/exo6.js
@@ -0,0 +1,9 @@
+class Rectangle {
+    constructor(longueur, largeur) {
+        this.longueur = longueur
+        this.largeur = largeur
+    }
+    calculerSurface() {
+        return this.longueur * this.largeur
+    }
+}
